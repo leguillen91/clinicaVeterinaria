4d49ce3bda0c7d92fb7b70e81d55c683bf725b4d
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-11-21 12:13:42 -0600

    login

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const path = require('path');
 const cors=require('cors');
-//const bodyParser= require('body-parser');
-const puerto=3003;
+const bodyParser= require('body-parser');
+const puerto=3002;
 
 const app = express();
 
+// Configura el motor de vistas Pug
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname,  'views'));
+
+// Ruta para renderizar la vista
+app.get('/home', (req, res) => {
+res.render('home');
+});
+
+
+app.get('/login', (req, res) => {
+	res.render('login');
+	});
+
 //mildrewers
 app.use(cors());
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/', (req, res) => {
-    res.send('Probando ClinicaVeterinaria');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.listen(puerto, () => {
 	console.log(`Servidor Levantado en https://localhost:${puerto}`);
 });
+
