691bfdec6f3e1cd09795884f6ef8e19a1f6d443d
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-11-26 13:48:43 -0600

    conexion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const path = require('path');
 const cors=require('cors');
 const bodyParser= require('body-parser');
@@ -27,7 +28,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
+
+
 app.listen(puerto, () => {
 	console.log(`Servidor Levantado en https://localhost:${puerto}`);
 });
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'clinicaVeterinaria'
+  });
+  
+
+  connection.connect((err) => {
+	if (err) {
+	  console.error('Error al conectar: ' + err.stack);
+	  return;
+	}
+	console.log('Conexión exitosa con ID: ' + connection.threadId);
+	// Aquí puedes realizar consultas, operaciones en la base de datos, etc.
+  });
+  
+  connection.query('SELECT * FROM cliente', (error, results, fields) => {
+	if (error) throw error;
+	console.log('Los resultados de la consulta son: ', results);
+  });
+  
